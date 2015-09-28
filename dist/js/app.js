c6225b139a0fa288e3b6fea68bd2d525af0bb3b2
Valdas Kalvaitis <valdas@kalvaitis.eu>
2015-09-28 16:04:10 +0300

    847d06569e01a6fa5f485f0044238b80308e6753

--- dist/js/app.js
+++ dist/js/app.js
@@ -428,77 +428,6 @@
     }
 
 })();
-(function () {
-    angular
-        .module('xApp')
-        .directive('clippy', dir);
-
-    function dir() {
-        return {
-            restrict: 'E',
-            controller: function() {
-                var enabled = localStorage.getItem('clippy') || false;
-
-                if (String(enabled).toLowerCase() == 'true') {
-                    runClippy();
-                }
-            }
-        };
-    }
-
-    function runClippy() {
-        clippy.load('Clippy', function(agent){
-            agent.show();
-            agent.reposition = function () {
-                if (!this._el.is(':visible')) return;
-                var o = this._el.offset();
-                var bH = this._el.outerHeight();
-                var bW = this._el.outerWidth();
-
-                var wW = $(window).width();
-                var wH = $(window).height();
-                var sT = $(window).scrollTop();
-                var sL = $(window).scrollLeft();
-                var top = o.top - sT;
-                var left = o.left - sL;
-                var m = 5;
-                if (top - m < 0) {
-                    top = m;
-                    this.hide();
-                    clearInterval(loop);
-                    this._balloon.hide(true);
-                } else if ((top + bH + m) > wH) {
-                    this.hide();
-                    clearInterval(loop);
-                    this._balloon.hide(true);
-                    top = wH - bH - m;
-                }
-
-                if (left - m < 0) {
-                    this.hide();
-                    clearInterval(loop);
-                    this._balloon.hide(true);
-                    left = m;
-                } else if (left + bW + m > wW) {
-                    this.hide();
-                    clearInterval(loop);
-                    this._balloon.hide(true);
-                    left = wW - bW - m;
-                }
-
-                this._el.css({left:left, top:top});
-                // reposition balloon
-                this._balloon.reposition();
-            };
-            agent._balloon.WORD_SPEAK_TIME = 200;
-            agent._balloon.CLOSE_BALLOON_DELAY = 15000;
-            var loop = setInterval(function () {
-                agent.speak(fortunes[Math.floor(Math.random()*fortunes.length)]);
-            }, 30000);
-        });
-    }
-})();
-
 (function() {
     angular
         .module('xApp')
@@ -2300,8 +2229,6 @@ var Password = {
             repeat: ''
         };
 
-        $scope.clippy = String(localStorage.getItem('clippy')) == 'false';
-
         $scope.ok = function() {
             Api.profile.save($scope.profile,
                 function() {
@@ -2309,10 +2236,6 @@ var Password = {
                     $modalInstance.close();
                 }
             );
-        };
-
-        $scope.toggleClippy = function() {
-            localStorage.setItem('clippy', $scope.clippy);
         };
 
         $scope.cancel = function () {
@@ -2326,7 +2249,7 @@ var Password = {
         .module('xApp')
         .controller('UserListController', controller);
 
-    function controller($scope, $modal, $timeout, toaster, Api, AuthFactory, users) {
+    function controller($scope, $modal, Api, users) {
         $scope.users = users;
 
         $scope.createUser = function() {

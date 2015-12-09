e398f086bdad284152b3c1a28b67f5206c28b06b
Valdas Kalvaitis <valdas@kalvaitis.eu>
2015-12-09 11:07:09 +0200

    b158dd268a52caa9bb6d59af95ed9f142ed5b7e2

--- dist/js/app.js
+++ dist/js/app.js
@@ -1961,6 +1961,45 @@ var Password = {
 (function() {
     angular
         .module('xApp')
+        .controller('ProjectTeamController', teamController);
+
+    function teamController($scope, Api, teams, project, access) {
+        $scope.teams = teams;
+        $scope.access = access;
+        $scope.project = project;
+
+        $scope.canAccess = function(team) {
+            return getAccessIndexForUserId(team.id) != -1;
+        };
+
+        $scope.grant = function(team) {
+            Api.projectTeams.save({
+                team_id: team.id,
+                project_id: $scope.project.id
+            }, function (response) {
+                $scope.access.push(response);
+            });
+        };
+
+        $scope.revoke = function(team) {
+            var accessIndex = getAccessIndexForUserId(team.id);
+
+            Api.projectTeams.delete({
+                id: $scope.access[accessIndex].id
+            }, function() {
+                $scope.access.splice(accessIndex, 1);
+            });
+        };
+
+        function getAccessIndexForUserId(teamId) {
+            return $scope.access.map(function (e) { return e.team_id; }).indexOf(teamId);
+        }
+    }
+})();
+
+(function() {
+    angular
+        .module('xApp')
         .controller('createTeamController', createTeamController);
 
     function createTeamController($scope, $modalInstance, Api) {
@@ -2130,45 +2169,6 @@ var Password = {
 
         function cancel() {
             $modalInstance.dismiss();
-        }
-    }
-})();
-
-(function() {
-    angular
-        .module('xApp')
-        .controller('ProjectTeamController', teamController);
-
-    function teamController($scope, Api, teams, project, access) {
-        $scope.teams = teams;
-        $scope.access = access;
-        $scope.project = project;
-
-        $scope.canAccess = function(team) {
-            return getAccessIndexForUserId(team.id) != -1;
-        };
-
-        $scope.grant = function(team) {
-            Api.projectTeams.save({
-                team_id: team.id,
-                project_id: $scope.project.id
-            }, function (response) {
-                $scope.access.push(response);
-            });
-        };
-
-        $scope.revoke = function(team) {
-            var accessIndex = getAccessIndexForUserId(team.id);
-
-            Api.projectTeams.delete({
-                id: $scope.access[accessIndex].id
-            }, function() {
-                $scope.access.splice(accessIndex, 1);
-            });
-        };
-
-        function getAccessIndexForUserId(teamId) {
-            return $scope.access.map(function (e) { return e.team_id; }).indexOf(teamId);
         }
     }
 })();
